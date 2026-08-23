1b8d0a8f6a993665199d4406a520f6ed84a51dfc
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-08-23 11:52:29 +0300

    fix: align Firebase runtime modules to V12

--- js/firebase.js
+++ js/firebase.js
@@ -1,76 +1,32 @@
 // ======================================================
-// MES CORE V27 Enterprise
+// MES CORE V28 Enterprise
 // File: /js/firebase.js
 // ======================================================
 
-import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-
-import {
-    initializeFirestore,
-    persistentLocalCache,
-    persistentMultipleTabManager
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-
-import {
-    getStorage
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
-
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
-
     apiKey: "YOUR_API_KEY",
-
     authDomain: "YOUR_PROJECT.firebaseapp.com",
-
     projectId: "YOUR_PROJECT",
-
     storageBucket: "YOUR_PROJECT.appspot.com",
-
     messagingSenderId: "000000000",
-
     appId: "YOUR_APP_ID"
-
 };
 
-const app = getApps().length
-    ? getApps()[0]
-    : initializeApp(firebaseConfig);
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 const db = initializeFirestore(app, {
-
     localCache: persistentLocalCache({
-
         tabManager: persistentMultipleTabManager()
-
     })
-
 });
 
 const storage = getStorage(app);
 const auth = getAuth(app);
-export {
 
-    app,
-
-    auth,
-
-    db,
-
-    storage
-
-};
-
-export default {
-
-    app,
-
-    auth,
-
-    db,
-
-    storage
-
-};
+export { app, auth, db, storage };
+export default { app, auth, db, storage };
